89faa30ed50ba7730efe44a7414d19eb1208de59
Kelly Hansen <kellyhansen182@gmail.com>
2021-02-19 20:08:24 -0800

    tableFields array data

--- src/AveragesTable.js
+++ src/AveragesTable.js
@@ -1,4 +1,111 @@
 function AveragesTable() {
+  const tableFields = [
+    {
+      displayName: 'Pos',
+      dataKey: 'split'
+    },
+    {
+      displayName: 'Age',
+      dataKey: 'age'
+    },
+    {
+      displayName: 'Pros Age',
+      dataKey: 'prospect_age'
+    },
+    {
+      displayName: 'AVG',
+      dataKey: 'avg'
+    },
+    {
+      displayName: 'OBP',
+      dataKey: 'obp'
+    },
+    {
+      displayName: 'SLG',
+      dataKey: 'slg'
+    },
+    {
+      displayName: 'OPS',
+      dataKey: 'ops'
+    },
+    {
+      displayName: 'TM OPS',
+      dataKey: 'tm_ops'
+    },
+    {
+      displayName: 'wOBA',
+      dataKey: 'woba'
+    },
+    {
+      displayName: 'BABIP',
+      dataKey: 'babip'
+    },
+    {
+      displayName: 'Exit Velo',
+      dataKey: 'avg_exit_velo'
+    },
+    {
+      displayName: '% > 90mph',
+      dataKey: 'exit_velo_90_plus'
+    },
+    {
+      displayName: '% > 100mph',
+      dataKey: 'exit_velo_100_plus'
+    },
+    {
+      displayName: 'Sw',
+      dataKey: 'swing_per'
+    },
+    {
+      displayName: 'Sw In Zone',
+      dataKey: 'swing_per_kzone'
+    },
+    {
+      displayName: 'Ch',
+      dataKey: 'chase_per'
+    },
+    {
+      displayName: 'Ch 2K',
+      dataKey: 'chase_per_2k'
+    },
+    {
+      displayName: 'Ms',
+      dataKey: 'miss_per'
+    },
+    {
+      displayName: 'Ms Ch',
+      dataKey: 'miss_chase_per'
+    },
+    {
+      displayName: 'G/F',
+      dataKey: 'gb_per_fb'
+    },
+    {
+      displayName: 'AB/HR',
+      dataKey: 'ab_per_hr'
+    },
+    {
+      displayName: 'AB/UBB',
+      dataKey: 'ab_per_ubb'
+    },
+    {
+      displayName: 'AB/SO',
+      dataKey: 'ab_per_so'
+    },
+    {
+      displayName: 'GB%',
+      dataKey: 'gb_per'
+    },
+    {
+      displayName: 'UBB%',
+      dataKey: 'ubb_per'
+    },
+    {
+      displayName: 'K%',
+      dataKey: 'so_per'
+    }
+  ]
+
   return (
     <div className="table-container">
       <div className="table-wrapper">
@@ -14,7 +121,6 @@ function AveragesTable() {
               <th>OPS</th>
               <th>TM OPS</th>
               <th>wOBA</th>
-              <th>BABIP</th>
               <th>BABIP</th>
               <th>Exit Velo</th>
               <th>% > 90mph</th>
@@ -34,6 +140,9 @@ function AveragesTable() {
               <th>K%</th>
             </tr>
           </thead>
+          <tbody>
+            <tr></tr>
+          </tbody>
         </table>
       </div>
     </div>
