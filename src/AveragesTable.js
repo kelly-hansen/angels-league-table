ddb0734e38cdb285be86f34683396ede8008613a
Kelly Hansen <kellyhansen182@gmail.com>
2021-02-19 21:17:41 -0800

    display proper table data for MLB/AAA leagues, styling

--- src/AveragesTable.js
+++ src/AveragesTable.js
@@ -24,95 +24,117 @@ function AveragesTable(props) {
     },
     {
       displayName: 'OBP',
-      dataKey: 'obp'
+      dataKey: 'obp',
+      displayNum: num => num.toFixed(3).toString(10).substring(1)
     },
     {
       displayName: 'SLG',
-      dataKey: 'slg'
+      dataKey: 'slg',
+      displayNum: num => num.toFixed(3).toString(10).substring(1)
     },
     {
       displayName: 'OPS',
-      dataKey: 'ops'
+      dataKey: 'ops',
+      displayNum: num => num.toFixed(3).toString(10).substring(1)
     },
     {
       displayName: 'TM OPS',
-      dataKey: 'tm_ops'
+      dataKey: 'tm_ops',
+      displayNum: num => num.toFixed(3).toString(10).substring(1)
     },
     {
       displayName: 'wOBA',
-      dataKey: 'woba'
+      dataKey: 'woba',
+      displayNum: num => num.toFixed(3).toString(10).substring(1)
     },
     {
       displayName: 'BABIP',
-      dataKey: 'babip'
+      dataKey: 'babip',
+      displayNum: num => num.toFixed(3).toString(10).substring(1)
     },
     {
       displayName: 'Exit Velo',
-      dataKey: 'avg_exit_velo'
+      dataKey: 'avg_exit_velo',
+      displayNum: num => num.toFixed(1)
     },
     {
       displayName: '% > 90mph',
-      dataKey: 'exit_velo_90_plus'
+      dataKey: 'exit_velo_90_plus',
+      displayNum: num => (num * 100).toFixed(0) + '%'
     },
     {
       displayName: '% > 100mph',
-      dataKey: 'exit_velo_100_plus'
+      dataKey: 'exit_velo_100_plus',
+      displayNum: num => (num * 100).toFixed(0) + '%'
     },
     {
       displayName: 'Sw',
-      dataKey: 'swing_per'
+      dataKey: 'swing_per',
+      displayNum: num => (num * 100).toFixed(0) + '%'
     },
     {
       displayName: 'Sw In Zone',
-      dataKey: 'swing_per_kzone'
+      dataKey: 'swing_per_kzone',
+      displayNum: num => (num * 100).toFixed(0) + '%'
     },
     {
       displayName: 'Ch',
-      dataKey: 'chase_per'
+      dataKey: 'chase_per',
+      displayNum: num => (num * 100).toFixed(0) + '%'
     },
     {
       displayName: 'Ch 2K',
-      dataKey: 'chase_per_2k'
+      dataKey: 'chase_per_2k',
+      displayNum: num => (num * 100).toFixed(0) + '%'
     },
     {
       displayName: 'Ms',
-      dataKey: 'miss_per'
+      dataKey: 'miss_per',
+      displayNum: num => (num * 100).toFixed(0) + '%'
     },
     {
       displayName: 'Ms Ch',
-      dataKey: 'miss_chase_per'
+      dataKey: 'miss_chase_per',
+      displayNum: num => (num * 100).toFixed(0) + '%'
     },
     {
       displayName: 'G/F',
-      dataKey: 'gb_per_fb'
+      dataKey: 'gb_per_fb',
+      displayNum: num => num.toFixed(1)
     },
     {
       displayName: 'AB/HR',
-      dataKey: 'ab_per_hr'
+      dataKey: 'ab_per_hr',
+      displayNum: num => num.toFixed(1)
     },
     {
       displayName: 'AB/UBB',
-      dataKey: 'ab_per_ubb'
+      dataKey: 'ab_per_ubb',
+      displayNum: num => num.toFixed(1)
     },
     {
       displayName: 'AB/SO',
-      dataKey: 'ab_per_so'
+      dataKey: 'ab_per_so',
+      displayNum: num => num.toFixed(1)
     },
     {
       displayName: 'GB%',
-      dataKey: 'gb_per'
+      dataKey: 'gb_per',
+      displayNum: num => (num * 100).toFixed(0) + '%'
     },
     {
       displayName: 'UBB%',
-      dataKey: 'ubb_per'
+      dataKey: 'ubb_per',
+      displayNum: num => (num * 100).toFixed(0) + '%'
     },
     {
       displayName: 'K%',
-      dataKey: 'so_per'
+      dataKey: 'so_per',
+      displayNum: num => (num * 100).toFixed(0) + '%'
     }
   ];
 
-  const positions = ['overall', 'AL', 'NL', 'C', '1B', '2B', '3B', 'SS', 'LF', 'CF', 'RF', 'DH'];
+  const positions = ['Overall', props.league === 'MLB' ? 'AL' : 'INT', props.league === 'MLB' ? 'NL' : 'PCL', 'C', '1B', '2B', '3B', 'SS', 'LF', 'CF', 'RF', 'DH'];
 
   return (
     <div className="table-container">
@@ -131,14 +153,14 @@ function AveragesTable(props) {
                 <tr key={pos}>
                   <td className="pos">{pos}</td>
                   {tableFields.map((field, ind) => {
-                    if (ind !== 0 && ind < 4) {
+                    if (ind !== 0) {
                       for (let i = 0; i < LeagueAvg.length; i++) {
                         if (LeagueAvg[i].league === props.league && LeagueAvg[i].split === pos) {
                           return <td key={pos + field.dataKey}>{field.displayNum(LeagueAvg[i][field.dataKey])}</td>;
                         }
                       }
                     }
-                    return;
+                    return false;
                   })}
                 </tr>
               );
