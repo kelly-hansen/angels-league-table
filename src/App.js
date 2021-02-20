f0c2042e5d587b68fac6b2373c1029240450902b
Kelly Hansen <kellyhansen182@gmail.com>
2021-02-19 16:39:41 -0800

    League nav

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
       <header>
         <h2>LEAGUE AVERAGES</h2>
       </header>
+      <nav>
+        <p className="league-title selected">LEAGUE1</p>
+        <p className="league-title">LEAGUE2</p>
+      </nav>
       {console.log(LeagueAvg)}
     </div>
   );
