971bd32709cf9808efadf8088bdd34f40dac35b6
Kelly Hansen <kellyhansen182@gmail.com>
2021-02-19 20:11:24 -0800

    use .map to render table headers

--- src/AveragesTable.js
+++ src/AveragesTable.js
@@ -112,32 +112,9 @@ function AveragesTable() {
         <table>
           <thead>
             <tr>
-              <th>Pos</th>
-              <th>Age</th>
-              <th>Pros Age</th>
-              <th>AVG</th>
-              <th>OBP</th>
-              <th>SLG</th>
-              <th>OPS</th>
-              <th>TM OPS</th>
-              <th>wOBA</th>
-              <th>BABIP</th>
-              <th>Exit Velo</th>
-              <th>% > 90mph</th>
-              <th>% > 100mph</th>
-              <th>Sw</th>
-              <th>Sw In Zone</th>
-              <th>Ch</th>
-              <th>Ch 2K</th>
-              <th>Ms</th>
-              <th>Ms Ch</th>
-              <th>G/F</th>
-              <th>AB/HR</th>
-              <th>AB/UBB</th>
-              <th>AB/SO</th>
-              <th>GB%</th>
-              <th>UBB%</th>
-              <th>K%</th>
+              {tableFields.map(field => {
+                return <th key={field.dataKey}>{field.displayName}</th>
+              })}
             </tr>
           </thead>
           <tbody>
