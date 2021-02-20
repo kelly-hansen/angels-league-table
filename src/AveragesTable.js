a75e6cb1d96ae830e17404ceb1929a6ac41a8405
Kelly Hansen <kellyhansen182@gmail.com>
2021-02-19 17:37:11 -0800

    thead and styling

--- src/AveragesTable.js
+++ src/AveragesTable.js
@@ -1,9 +1,41 @@
 function AveragesTable() {
   return (
-    <div className="table-cont">
-      <table>
-        <p>Hello</p>
-      </table>
+    <div className="table-container">
+      <div className="table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th>Pos</th>
+              <th>Age</th>
+              <th>Pros Age</th>
+              <th>AVG</th>
+              <th>OBP</th>
+              <th>SLG</th>
+              <th>OPS</th>
+              <th>TM OPS</th>
+              <th>wOBA</th>
+              <th>BABIP</th>
+              <th>BABIP</th>
+              <th>Exit Velo</th>
+              <th>% > 90mph</th>
+              <th>% > 100mph</th>
+              <th>Sw</th>
+              <th>Sw In Zone</th>
+              <th>Ch</th>
+              <th>Ch 2K</th>
+              <th>Ms</th>
+              <th>Ms Ch</th>
+              <th>G/F</th>
+              <th>AB/HR</th>
+              <th>AB/UBB</th>
+              <th>AB/SO</th>
+              <th>GB%</th>
+              <th>UBB%</th>
+              <th>K%</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     </div>
   );
 }
